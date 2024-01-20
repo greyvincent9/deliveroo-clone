bab21fd11895ef1341a543fbb2587ee2adb0c971
Vincent Ang <greyvincent9@gmail.com>
2024-01-20 15:10:38 +0800

    added new search icon

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, TextInput, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import {
+	UserIcon,
+	ChevronDownIcon,
+	MagnifyingGlassIcon,
+	AdjustmentsVerticalIcon,
+} from "react-native-heroicons/outline";
+import Categories from "../components/categories";
 
 const HomeScreen = () => {
 	const navigation = useNavigation();
@@ -13,8 +20,51 @@ const HomeScreen = () => {
 	}, []);
 
 	return (
-		<SafeAreaView>
-			<Text className="text-blue-500">Hello</Text>
+		<SafeAreaView className="bg-white pt-2">
+			<View>
+				{/* Header */}
+				<View className="flex-row pb-3 items-center mx-4 space-x-2">
+					<Image
+						source={{
+							uri: "https://links.papareact.com/wru",
+						}}
+						className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+					/>
+
+					<View className="flex-1 ">
+						<Text className="font-bold text-gray-400 text-xs">
+							Deliver Now!
+						</Text>
+
+						<Text className="font-bold text-xl">
+							Current Location
+							<ChevronDownIcon size={20} color="#00CCBB" />
+						</Text>
+					</View>
+
+					<UserIcon size={35} color="#00CCBB" />
+				</View>
+
+				{/* Search */}
+				<View className="flex-row items-center space-x-2 pb-2 mx-4">
+					<View className="flex-row items-center flex-1 space-x-2 bg-gray-200 p-2">
+						<MagnifyingGlassIcon size={20} color="gray" />
+						<TextInput
+							placeholder="Restaurants and cuisines"
+							keyboardType="default"
+						/>
+					</View>
+					<AdjustmentsVerticalIcon color="#00CCBB" />
+				</View>
+
+				{/* Body */}
+				<ScrollView className="bg-gray-100" contentContainerStyle={{}}>
+					{/* Categories */}
+					<Categories />
+
+					{/* Featured Rows*/}
+				</ScrollView>
+			</View>
 		</SafeAreaView>
 	);
 };
