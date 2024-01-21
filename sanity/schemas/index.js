85dbf908c4cb3b63374fc1c0270e4d054c892aeb
Vincent Ang <greyvincent9@gmail.com>
2024-01-21 14:47:58 +0800

    Created featured rows and implemented Sanity.io

---
+++ sanity/schemas/index.js
@@ -0,0 +1,7 @@
+// schemas/index.js
+import category from './category'
+import restaurant from './restaurant'
+import dish from './dish'
+import featured from './featured'
+
+export const schemaTypes = [restaurant, dish, category, featured]
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,7 +8,8 @@ import {
 	MagnifyingGlassIcon,
 	AdjustmentsVerticalIcon,
 } from "react-native-heroicons/outline";
-import Categories from "../components/categories";
+import Categories from "../components/Categories";
+import FeaturedRow from "../components/FeaturedRow";
 
 const HomeScreen = () => {
 	const navigation = useNavigation();
@@ -21,50 +22,75 @@ const HomeScreen = () => {
 
 	return (
 		<SafeAreaView className="bg-white pt-2">
-			<View>
-				{/* Header */}
-				<View className="flex-row pb-3 items-center mx-4 space-x-2">
-					<Image
-						source={{
-							uri: "https://links.papareact.com/wru",
-						}}
-						className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-					/>
+			{/* Header */}
+			<View className="flex-row pb-3 items-center mx-4 space-x-2">
+				<Image
+					source={{
+						uri: "https://links.papareact.com/wru",
+					}}
+					className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+				/>
 
-					<View className="flex-1 ">
-						<Text className="font-bold text-gray-400 text-xs">
-							Deliver Now!
-						</Text>
+				<View className="flex-1 ">
+					<Text className="font-bold text-gray-400 text-xs">
+						Deliver Now!
+					</Text>
 
-						<Text className="font-bold text-xl">
-							Current Location
-							<ChevronDownIcon size={20} color="#00CCBB" />
-						</Text>
-					</View>
-
-					<UserIcon size={35} color="#00CCBB" />
+					<Text className="font-bold text-xl">
+						Current Location
+						<ChevronDownIcon size={20} color="#00CCBB" />
+					</Text>
 				</View>
 
-				{/* Search */}
-				<View className="flex-row items-center space-x-2 pb-2 mx-4">
-					<View className="flex-row items-center flex-1 space-x-2 bg-gray-200 p-2">
-						<MagnifyingGlassIcon size={20} color="gray" />
-						<TextInput
-							placeholder="Restaurants and cuisines"
-							keyboardType="default"
-						/>
-					</View>
-					<AdjustmentsVerticalIcon color="#00CCBB" />
-				</View>
-
-				{/* Body */}
-				<ScrollView className="bg-gray-100" contentContainerStyle={{}}>
-					{/* Categories */}
-					<Categories />
-
-					{/* Featured Rows*/}
-				</ScrollView>
+				<UserIcon size={35} color="#00CCBB" />
 			</View>
+
+			{/* Search */}
+			<View className="flex-row items-center space-x-2 pb-2 mx-4">
+				<View className="flex-row items-center flex-1 space-x-2 bg-gray-200 p-2">
+					<MagnifyingGlassIcon size={20} color="gray" />
+					<TextInput
+						placeholder="Restaurants and cuisines"
+						keyboardType="default"
+					/>
+				</View>
+				<AdjustmentsVerticalIcon color="#00CCBB" />
+			</View>
+
+			{/* Body */}
+			<ScrollView
+				className="bg-gray-100"
+				contentContainerStyle={{
+					paddingBottom: 100,
+				}}
+			>
+				{/* Categories */}
+				<Categories />
+
+				{/* Featured Rows*/}
+				<FeaturedRow
+					id="123"
+					title="Featured"
+					description="Paid placements from our partners"
+					featuredCategory="featured"
+				/>
+
+				{/* Tasty Discounts */}
+				<FeaturedRow
+					id="1234"
+					title="Tasty Discounts"
+					description="Everyone's been enjoying these juciy discounts!"
+					featuredCategory="discounts"
+				/>
+
+				{/* Offers near you */}
+				<FeaturedRow
+					id="12345"
+					title="Offers near you"
+					description="Why not support your local restaurants tonight!"
+					featuredCategory="offers"
+				/>
+			</ScrollView>
 		</SafeAreaView>
 	);
 };
